ab56308f6341f373b9739a695b49cb186fd5de67
KudoByte29 <a.valenciacontreras@uandresbello.edu>
2026-08-13 07:04:30 -0400

    a

--- index.php
+++ index.php
@@ -11,7 +11,9 @@
         <!--Navbar-->
         <nav class="navbar navbar-expand-sm navbar-dark bg-dark">
         <div class="container-fluid">
-            <a class="navbar-brand" href="javascript:void(0)">Logo</a>
+            <a class="navbar-brand" href="index.php">
+                <img src="img/logo.png" alt="Logo" height="50">
+            </a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#mynavbar">
             <span class="navbar-toggler-icon"></span>
             </button>
