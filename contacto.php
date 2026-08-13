7f47a98e418b0f342b6dbb57c0416a45672cb524
KudoByte29 <a.valenciacontreras@uandresbello.edu>
2026-08-13 05:42:22 -0400

    git remote add origin https://github.com/KudoByte29/pw2026.git git branch -M main git push -u origin main

---
+++ contacto.php
@@ -0,0 +1,15 @@
+<html>
+    <head>
+        <title>Nuestro Contacto</title>
+    </head>
+    <body>
+        <!--Navbar-->
+
+        <!--Container-->
+        Nuestro Contacto<br>
+        <a href="index.php">Ir a la Página Principal</a><br>
+        <a href="quienessomos.php">¿Quiénes somos?</a><br>
+        <a href="productos.php">Productos</a><br>
+        <a href="novedades.php">Novedades</a><br>
+        <a href="ofertas.php">Ofertas</a><br>
+        <a href="contacto.php">Contacto</a><br>
---
+++ novedades.php
@@ -0,0 +1,15 @@
+<html>
+    <head>
+        <title>Novedades</title>
+    </head>
+    <body>
+        <!--Navbar-->
+
+        <!--Container-->
+        Novedades<br>
+        <a href="index.php">Ir a la Página Principal</a><br>
+        <a href="quienessomos.php">¿Quiénes somos?</a><br>
+        <a href="productos.php">Productos</a><br>
+        <a href="novedades.php">Novedades</a><br>
+        <a href="ofertas.php">Ofertas</a><br>
+        <a href="contacto.php">Contacto</a><br>
