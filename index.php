ee489d645a8165448a22174585f59fb0fad00461
KudoByte29 <a.valenciacontreras@uandresbello.edu>
2026-08-13 07:00:58 -0400

    a

--- index.php
+++ index.php
@@ -40,10 +40,6 @@
             </div>
         </div>
         </nav>
-        <div class="container-fluid mt-3">
-        <h3>Navbar Forms</h3>
-        <p>You can also include forms inside the navigation bar.</p>
-        </div>
 
         <!--Container-->    
         <div class="container-fluid bg-warning">
