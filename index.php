129c600f1b9e046d80a284f3b2f7194a09e5db31
KudoByte29 <a.valenciacontreras@uandresbello.edu>
2026-08-13 06:34:31 -0400

    a

--- index.php
+++ index.php
@@ -1,15 +1,29 @@
-<html>
+<!DOCTYPE html>
+<html lang="es">
     <head>
         <title>Página Principal</title>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet">
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js"></script>
     </head>
     <body>
         <!--Navbar-->
 
-        <!--Container-->
+        <!--Container-->    
         Pagina Principal<br>
-        <a href="index.php">Ir a la Página Principal</a><br>
-        <a href="quienessomos.php">¿Quiénes somos?</a><br>
-        <a href="productos.php">Productos</a><br>
-        <a href="novedades.php">Novedades</a><br>
-        <a href="ofertas.php">Ofertas</a><br>
-        <a href="contacto.php">Contacto</a><br>
+        <div class="container-fluid bg-warning">
+            <a href="index.php">Ir a la Página Principal</a><br>
+            <a href="quienessomos.php">¿Quiénes somos?</a><br>
+            <a href="productos.php">Productos</a><br>
+            <a href="novedades.php">Novedades</a><br>
+            <a href="ofertas.php">Ofertas</a><br>
+            <a href="contacto.php">Contacto</a><br>
+        </div>
+
+
+        <!--Footer-->
+        
+        <!--Modal-->
+    </body>
+</html>
