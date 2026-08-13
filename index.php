e80071e318c3ae9434c361f85c6d1de744cc2167
KudoByte29 <a.valenciacontreras@uandresbello.edu>
2026-08-13 16:38:34 -0400

    a

--- index.php
+++ index.php
@@ -36,6 +36,9 @@
                     </li>
                 </ul>
             </div>
+                <li class="nav-item">
+                    <a class="nav-link text-white" href="tucuenta.php">Mi Cuenta</a>
+                </li>
             <button type="button" class="btn btn-outline-light" data-bs-toggle="modal" data-bs-target="#myModal">Acceder</button>
         </div>
         </nav>
@@ -61,16 +64,31 @@
                 <div class="modal-content">
                     <!-- Modal Header -->
                     <div class="modal-header">
-                        <h4 class="modal-title">Accede a tu cuenta</h4>
+                        <h4 class="modal-title">Accede a tu Cuenta</h4>
                         <button type="button" class="btn-close" data-bs-dismiss="modal"></button>
                     </div>
                     <!-- Modal body -->
                     <div class="modal-body">
-                        Modal body..
+                    <form action="/action_page.php">
+                    <div class="mb-3 mt-3">
+                        <label for="email" class="form-label">Email:</label>
+                        <input type="email" class="form-control" id="email" placeholder="Enter email" name="email">
+                    </div>
+                    <div class="mb-3">
+                        <label for="pwd" class="form-label">Contraseña:</label>
+                        <input type="password" class="form-control" id="pwd" placeholder="Enter password" name="pswd">
+                    </div>
+                    <div class="form-check mb-3">
+                        <label class="form-check-label">
+                        <input class="form-check-input" type="checkbox" name="remember"> Recordarme
+                        </label>
+                    </div>
+                    <button type="submit" class="btn btn-primary">Submit</button>
+                    </form>
                     </div>
                     <!-- Modal footer -->
                     <div class="modal-footer">
-                        <button type="button" class="btn btn-danger" data-bs-dismiss="modal">Close</button>
+                        <button type="button" class="btn btn-danger" data-bs-dismiss="modal">Cerrar</button>
                     </div>
                 </div>
             </div>
